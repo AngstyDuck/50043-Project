ac3758563711ec3e78b9c935e646a9322cd882cc
databasemod <solsticedante@gmail.com>
2019-10-14 00:17:13 +0700

    removed buggy module from requirements.txt; updated DIR of json used by metadataFill.py; added installation of git-lfs in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,27 +10,39 @@
 # git clone https://github.com/dinhtta/istd50043_project.git ~/Desktop/50043-Prof
 # chmod 777 ~/Desktop/50043-Prof/scripts/get_data.sh
 # ~/Desktop/50043-Prof/scripts/get_data.sh
+# rm -rf meta_Kindle_Store.json
+
 
 # Installing MySQL
 # apt install -y mysql-server
-# mysql_secure_installation --host=::1 --port=3307 --password=1234
 
 # Installing MongoDB
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-service mongod start
-
-
-# to install mongodb compass
-dpkg -i mongodb-compass_1.19.12_amd64.deb
-apt-get install -f
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# apt-get install -y mongodb-org
 
 
 # Install python mondules
-sudo apt-get install python3 python3-pip
-sudo apt-get install python3-venv
-python3 -m venv backend/venv
-source backend/venv/bin/activate
-pip3 install -r backend/requirements.txt
+# apt-get install -y python3 python3-pip
+# apt-get install -y python3-venv
+# python3 -m venv backend/venv
+# source backend/venv/bin/activate
+# pip3 install -r backend/requirements.txt
+
+
+# to install mongodb compass
+# dpkg -i installation_files/mongodb-compass_1.19.12_amd64.deb
+# apt-get install -f -y
+
+# installing git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+apt-get install -y git-lfs
+git lfs install
+
+# Cloning metadata (with the correct format) from LFS repo
+git clone https://github.com/SolsticeDante/50043-Project-lfs.git ~/Desktop/50043-Project-lfs
+
+
+# Insert kindle metadata into mongodb
+echo "Inserting kindle metadata into mongodb..."
+python3 ./installation_files/metadataFill.py
