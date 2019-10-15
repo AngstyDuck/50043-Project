29b2cf253b35f950d986117cfc56418a1e8afdb0
Kenneth Sim <kensim28@hotmail.com>
2019-10-15 19:05:54 +0800

    rough layout for tiles

--- frontend/ebook/src/App.vue
+++ frontend/ebook/src/App.vue
@@ -35,46 +35,6 @@
 
     <v-content>
       <router-view></router-view>
-      <!-- <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container> -->
     </v-content>
 
     <v-footer app>
--- frontend/ebook/src/views/BookList.vue
+++ frontend/ebook/src/views/BookList.vue
@@ -1,6 +1,55 @@
 <template>
-  <Book />
+<body>
+  <v-row no-gutters>
+    <v-col cols="12" sm="2">
+      <Book/>
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+  </v-row>
+    <v-row no-gutters>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+    <v-col cols="12" sm="2">
+      <Book />
+    </v-col>
+  </v-row>
+</body>
 </template>
+
+<style scoped>
+
+.book {
+  margin: 5px;
+}
+
+</style>
 
 <script>
 import Book from "../components/Book";
