38a5a1c99acc865caf3945deec1a464722ecf438
Kenneth Sim <kensim28@hotmail.com>
2019-10-28 03:45:10 +0800

    Comment pop-up design complete

--- frontend/ebook/src/components/Book.vue
+++ frontend/ebook/src/components/Book.vue
@@ -110,52 +110,50 @@
       <v-dialog v-model="dialog" persistent max-width="600px">
         <v-card>
           <v-card-title>
-            <span class="headline">User Profile</span>
+            <span class="headline">Post a review</span>
           </v-card-title>
           <v-card-text>
             <v-container>
+              <v-row no-gutters>
+                <v-col cols="12">Overall rating</v-col>
+              </v-row>
               <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field label="Legal first name*" required></v-text-field>
+                <v-col cols="12">
+                  <star-rating :star-size="40"></star-rating>
                 </v-col>
-                <v-col cols="12" sm="6" md="4">
+              </v-row>
+              <v-row>
+                <v-col cols="12">
                   <v-text-field
-                    label="Legal middle name"
-                    hint="example of helper text only on focus"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
+                    label="Add a headline"
                     required
+                    hint="What's the most important to know?"
+                    persistent-hint
                   ></v-text-field>
                 </v-col>
                 <v-col cols="12">
-                  <v-text-field label="Email*" required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field label="Password*" type="password" required></v-text-field>
+                  <v-textarea
+                    name="input-7-1"
+                    label="Write your review"
+                    hint="What did you like or dislike? Was the book what you expected?"
+                    persistent-hint
+                  ></v-textarea>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <v-autocomplete
-                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                    label="Interests"
-                    multiple
-                  ></v-autocomplete>
+                  <v-text-field
+                    label="Username"
+                    required
+                    hint="What do you want to be known as?"
+                    persistent-hint
+                  ></v-text-field>
                 </v-col>
               </v-row>
             </v-container>
-            <small>*indicates required field</small>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+            <v-btn color="blue darken-1" @click="dialog = false">Post Review</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
