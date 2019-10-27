1a51cf06241fd14398694bb340cbeedf29d6f202
Kenneth Sim <kensim28@hotmail.com>
2019-10-28 03:30:12 +0800

    Added mockup comment pop-up

--- frontend/ebook/src/components/Book.vue
+++ frontend/ebook/src/components/Book.vue
@@ -41,7 +41,7 @@
           </div>
         </v-col>
         <v-col>
-          <div class="subtitle">Comments</div>
+          <div class="subtitle">Reviews</div>
           <div class="average-title">
             Average rating:
             <star-rating
@@ -88,11 +88,12 @@
                           <v-card-actions>
                             <v-btn
                               depressed
-                              rounded
                               right
                               :disabled="review.helpfulDisable"
+                              large
+                              rounded
                               v-on:click="helpfulButton(index)"
-                            >Helpful</v-btn>
+                            >{{review.helpfulDisable?"Thank you":"Helpful"}}</v-btn>
                           </v-card-actions>
                         </v-col>
                       </v-row>
@@ -105,6 +106,60 @@
         </v-col>
       </v-row>
     </v-container>
+    <div>
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">User Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Legal first name*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Password*" type="password" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 
@@ -161,6 +216,7 @@ export default {
   },
   data: () => ({
     page: 1,
+    dialog: true,
     book: {
       asin: "B002HWRR78",
       price: 0.99,
