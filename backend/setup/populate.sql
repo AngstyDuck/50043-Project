e7b879b67228aa0e77b0165cb9288444d9c7ccf2
BillioJeverson <billio_jeverson@mymail.sutd.edu.sg>
2019-11-11 17:26:35 +0800

    Update populate.sql

--- backend/setup/populate.sql
+++ backend/setup/populate.sql
@@ -3,20 +3,21 @@ CREATE DATABASE amazon;
 use amazon;
 
 CREATE TABLE amazonreviews(
-	id INT NOT NULL AUTO_INCREMENT,
+	id BIGINT(20) NOT NULL AUTO_INCREMENT,
 	asin VARCHAR(255) NULL,
 	helpful VARCHAR(255) NULL,
-	overall INT NULL,
+	overall BIGINT(20) NULL,
 	reviewText VARCHAR(255) NULL,
 	reviewTime DATE NULL,
 	reviewerID VARCHAR(255) NULL,
 	reviewerName VARCHAR(255) NULL,
 	summary VARCHAR(255) NULL,
-	unixReviewTime INT NULL,
+	unixReviewTime BIGINT(20) NULL,
 	PRIMARY KEY (id)
 	);
 
 LOAD DATA LOCAL INFILE 'C:/Users/user/Documents/Big Database and Data system/Lab1/50043-Project/backend/setup/kindle_reviews.csv' 
 INTO TABLE amazonreviews FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS 
 ;
