7806a0fc4c881f51c94918094bd1db9be8e76eb4
angsty-duck <solsticedante@gmail.com>
2019-11-11 10:23:56 +0800

    made config info accessible through app.config

--- backend/reviews/reviews.py
+++ backend/reviews/reviews.py
@@ -1,15 +1,52 @@
 import os
-
+import sys
 from flask import Flask
+sys.path.insert(1, "./..")
+from config import Config
+
+
+
+
+
+app = Flask(__name__)
+
+"""
+app.config.update(
+            MONGO_HOST = self.MONGO_HOST,
+            MONGO_USERNAME = self.MONGO_USERNAME,
+            MONGO_PASSWORD = self.MONGO_PASSWORD,
+            
+            MYSQL_HOST = self.MYSQL_HOST,
+            MYSQL_USERNAME = self.MYSQL_USERNAME,
+            MYSQL_PASSWORD = self.MYSQL_PASSWORD,
+            MYSQL_DATABASE = self.MYSQL_DATABASE,
+            
+            mongo_client = self.mongo_client
+            
+            mongo_db_metadata = self.mongo_db_metadata
+            mongo_collection_metadata = self.mongo_collection_metadata
+            
+            mongo_db_logs = self.mongo_db_logs
+            mongo_collection_logs = self.mongo_collection_logs
+            
+            mongo_db_test = self.mongo_db_test
+            mongo_collection_test = self.mongo_collection_test
+            
+            mysql_pymysql_connection = self.mysql_pymysql_connection
+            
+            mysql_sqlalchemy_engine = self.mysql_sqlalchemy_engine
+        )
+"""
+
+import routes
+
+
 # from config import Config
 # from flask_pymongo import PyMongo
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_migrate import Migrate
-
-
 #SQL
-
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # app.config.from_object(Config)
 # db = SQLAlchemy(app)
@@ -27,6 +64,6 @@ app = Flask(__name__)
 # logActivity = PyMongo(app)
 
 # from app import routes, models
-import routes
+# import routes
 
 
