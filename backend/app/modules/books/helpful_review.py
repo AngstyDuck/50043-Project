e3e3a83c7c308a3424ab2b7b5166c533caf78f89
Ubuntu <ubuntu@ip-172-31-39-180.us-west-2.compute.internal>
2019-11-20 12:27:00 +0000

    helpful_review updated

--- backend/app/modules/books/helpful_review.py
+++ backend/app/modules/books/helpful_review.py
@@ -22,20 +22,20 @@ from datetime import datetime
 from flask import current_app as app
 
 def _helpful_review():
-    data = {
-    "reviewerID": "A3DE6XGZ2EPADS",
-    "asin": "B000F83SZQ",
-    "reviewerName": "WPY"
-    }
-    #data = request.json
-    getGuery = "SELECT * FROM {0} WHERE asin=\'{1}\', reviewerID=\'{2}\', reviewerName=\'{3}\' LIMIT 1 ".format(app.config["MYSQL_TABLE_REVIEWS"], data['asin'], data['reviewerID'],data['reviewerName'])
-    connection = app.config["PYMYSQL_CONNECTION"].cursor()
-    with connection as cursor:
+   # data = {
+   # "reviewerID": "A3DE6XGZ2EPADS",
+   # "asin": "B000F83SZQ",
+   # "reviewerName": "WPY"
+   # }
+    data = request.json
+    getQuery = "SELECT * FROM {0} WHERE asin=\'{1}\' and  reviewerID=\'{2}\' and reviewerName=\'{3}\' LIMIT 1 ".format(app.config["MYSQL_TABLE_REVIEWS"], data['asin'], data['reviewerID'],data['reviewerName'])
+    connection = app.config["PYMYSQL_CONNECTION"]
+    with connection.cursor() as cursor:
         cursor.execute(getQuery)
         query_result = cursor.fetchall()
         helpful_ = []
-        helpful_.append(int(query_result[0]["helpful_"][1])+1)
-        helpful_.append(int(query_result[0]["helpful_"][4])+1)
+        helpful_.append(int(query_result[0]["helpful"][1])+1)
+        helpful_.append(int(query_result[0]["helpful"][4])+1)
         cursor.execute("UPDATE {0} SET helpful = \'{1}\' WHERE id =\'{2}\'".format(app.config["MYSQL_TABLE_REVIEWS"],str(helpful_),query_result[0]["id"] ))
     connection.commit()
     return{"Update": "Done"}
