bd849768c654b1fe22e1d61331f546e2c6fcbff9
kensim48 <kensim28@hotmail.com>
2019-12-05 18:01:58 +0800

    Added mysql automation script

--- misc/build.sh
+++ misc/build.sh
@@ -1,7 +1,21 @@
-instance_id=`aws ec2 run-instances --image-id ami-061eb2b23f9f8839c --count 1 --instance-type t2.micro --key-name key1 --security-group-ids sg-e7cb1999 | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['InstanceId'])"`
-echo 'Waiting for VM to spin up'
-sleep '40' 
-aws ec2 associate-address --allocation-id eipalloc-0051798c2bac2680b --instance-id $instance_id
-public_dns_name=`aws ec2 describe-instances --instance-id $instance_id | python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicDnsName'])"`
+echo 'Creating 3 VMs'
+instances_json=`aws ec2 run-instances --image-id ami-061eb2b23f9f8839c --count 3 --instance-type t2.micro --key-name key1 --security-group-ids sg-e7cb1999`
+instance_id_frontendbackend=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['InstanceId'])"`
+instance_id_mysql=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][1]['InstanceId'])"`
+instance_id_mongodb=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][2]['InstanceId'])"`
+echo 'Waiting for VMs to spin up, waiting for 35s'
+sleep '35' 
+echo 'Associating IP address'
+aws ec2 associate-address --allocation-id eipalloc-0051798c2bac2680b --instance-id $instance_id_frontendbackend
+aws ec2 associate-address --allocation-id eipalloc-02bd26683a27f8c3d --instance-id $instance_id_mysql
+aws ec2 associate-address --allocation-id eipalloc-0a1a470d36591c123 --instance-id $instance_id_mongodb
+public_dns_frontendbackend=`aws ec2 describe-instances --instance-id $instance_id_frontendbackend | python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicDnsName'])"`
+public_dns_mysql=`aws ec2 describe-instances --instance-id $instance_id_mysql | python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicDnsName'])"`
+public_dns_mongodb=`aws ec2 describe-instances --instance-id $instance_id_mongodb | python3 -c "import sys, json; print(json.load(sys.stdin)['Reservations'][0]['Instances'][0]['PublicDnsName'])"`
 chmod 400 key1.pem
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "key1.pem" ubuntu@$public_dns_name 'bash -s' < frontendbackend.sh -yes
+echo 'SSH into mysql'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "key1.pem" ubuntu@$public_dns_frontendbackend 'bash -s' < frontendbackend.sh
+echo 'SSH into mongodb'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "key1.pem" ubuntu@$public_dns_mysql 'bash -s' < frontendbackend.sh
+echo 'SSH into frontend/backend'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "key1.pem" ubuntu@$public_dns_mongodb 'bash -s' < frontendbackend.sh
