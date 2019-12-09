bcfdf493334f511b0f2f42b578cbac7ecdece78a
Kenneth Sim <kensim28@hotmail.com>
2019-12-10 00:01:52 +0800

    Removed footer

--- frontend/ebook/src/App.vue
+++ frontend/ebook/src/App.vue
@@ -1,27 +1,6 @@
 <template>
   <div @scroll="scrolledToBottom" class="scrollDiv">
     <v-app id="inspire">
-      <!-- <v-navigation-drawer disable-resize-watcher v-model="drawer" app clipped>
-        <v-list dense>
-          <v-list-item @click>
-            <v-list-item-action>
-              <v-icon>mdi-view-dashboard</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Dashboard</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click>
-            <v-list-item-action>
-              <v-icon>mdi-settings</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Settings</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>-->
-
       <v-app-bar app clipped-left>
         <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
         <v-btn @click="goHome()" text icon color="blue lighten-2">
@@ -51,10 +30,6 @@
       <v-content>
         <router-view></router-view>
       </v-content>
-
-      <v-footer app>
-        <span>&copy; 2019</span>
-      </v-footer>
     </v-app>
   </div>
 </template>
