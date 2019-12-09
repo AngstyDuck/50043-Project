5a67f6dc71b414e372c024fd646dc0948a72e5dc
Kenneth Sim <kensim28@hotmail.com>
2019-12-10 00:28:50 +0800

    Added nginx files for docker vue routing - not tested

--- frontend/ebook/src/store/store.js
+++ frontend/ebook/src/store/store.js
@@ -1,8 +1,8 @@
 import axiosInit from "axios";
 
 const axios = axiosInit.create({
-//   baseURL: "http://BACKEND_IP_ADDR:5000",
-	baseURL: "http://127.0.0.1:5000",
+  baseURL: "http://BACKEND_IP_ADDR:5000",
+	// baseURL: "http://127.0.0.1:5000",
   headers: {
     "Content-Type": "application/json"
   }
