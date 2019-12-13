41f390224a2c3dcd4214af6457f14d4fe5eed57f
Ubuntu <ubuntu@ip-172-31-84-136.ec2.internal>
2019-12-13 07:07:06 +0000

    added config files for aws account

--- misc/aws_configure.sh
+++ misc/aws_configure.sh
@@ -3,24 +3,38 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $SCRIPTPATH
-PATH="/usr/local/bin:$PATH"
-# apt update
+
+# PATH="/usr/local/bin:$PATH"
+
+apt update
 apt install -y jq
+apt-get update
+apt-get install -y python3-pip
+
+: '
+# format of credentials.json
+{
+	"access_key_id": "123",
+	"secret_access_key": "456"
+}
+'
+
 
 aws configure <<EOF
-$(cat "${SCRIPTPATH}/aws_config.json" | jq -r ".access_key_id")
-$(cat "${SCRIPTPATH}/aws_config.json" | jq -r ".secret_access_key")
+$(cat "${SCRIPTPATH}/credentials.json" | jq -r ".access_key_id")
+$(cat "${SCRIPTPATH}/credentials.json" | jq -r ".secret_access_key")
 "ap-southeast-1"
 "json"
 y
 EOF
 
+
 # /home/xubuntu/.config/flintrock/config.yaml
 sudo -H pip3 install flintrock
-cp "${SCRIPTPATH}/config.yaml" "${HOME}/.config/flintrock/config.yaml"
+sed "s/TO_REPLACEE/${HOME}\/flint_cluster.pem/g" "${SCRIPTPATH}/config.yaml" > "${hHOME}/.config/flintrock/"
 
-export AWS_ACCESS_KEY_ID=$(cat "${SCRIPTPATH}/aws_config.json" | jq -r ".access_key_id")
-export AWS_SECRET_ACCESS_KEY=$(cat "${SCRIPTPATH}/aws_config.json" | jq -r ".secret_access_key")
+export AWS_ACCESS_KEY_ID=$(cat "${SCRIPTPATH}/credentials.json" | jq -r ".access_key_id")
+export AWS_SECRET_ACCESS_KEY=$(cat "${SCRIPTPATH}/credentials.json" | jq -r ".secret_access_key")
 echo $AWS_ACCESS_KEY_ID
 
 flintrock launch sample-cluster
