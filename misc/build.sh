d835f353344d44166ecf38ae9cc2476e3cc3b167
kensim48 <kensim28@hotmail.com>
2019-12-22 16:49:57 +0800

    Added automatic security group creation

--- misc/build.sh
+++ misc/build.sh
@@ -1,5 +1,9 @@
+echo 'Creating security group, if error during this part, make sure to remove db-sg security group from dashboard'
+sg_id=`aws ec2 create-security-group --group-name db-sg --description "Security group for DB"| python3 -c "import sys, json; print(json.load(sys.stdin)['GroupId'])"`
+aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol -1 --port -1 --cidr 0.0.0.0/0
+echo "Security group created, $sg_id"
 echo 'Creating 3 VMs'
-instances_json=`aws ec2 run-instances --image-id ami-061eb2b23f9f8839c --count 3 --instance-type t2.medium --key-name key1 --security-group-ids sg-e7cb1999`
+instances_json=`aws ec2 run-instances --image-id ami-061eb2b23f9f8839c --count 3 --instance-type t2.medium --key-name key1 --security-group-ids $sg_id`
 instance_id_frontendbackend=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][0]['InstanceId'])"`
 instance_id_mysql=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][1]['InstanceId'])"`
 instance_id_mongodb=`echo $instances_json | python3 -c "import sys, json; print(json.load(sys.stdin)['Instances'][2]['InstanceId'])"`
